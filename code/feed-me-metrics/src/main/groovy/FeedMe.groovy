d2e267fdc8e1fc9f43b55bb76e1ba9034789f9ca
Leonard Axelsson <leo@mojang.com>
2012-01-30 16:56:34 +0100

    Added different servers to the example.

--- code/feed-me-metrics/src/main/groovy/FeedMe.groovy
+++ code/feed-me-metrics/src/main/groovy/FeedMe.groovy
@@ -17,9 +17,11 @@ class FeedMe {
     }
     
     def initializeMetricCreators() {
-        ['monster.queue': createAllTimeQueue(),
-         'logins.successful': createMoving(100, 100),
-         'logins.failed': createMoving(10, 50)]
+        ['monster.darth.queue': createAllTimeQueue(),
+         'logins.leia.successful': createMoving(100, 100),
+         'logins.luke.successful': createMoving(100, 100),
+         'logins.jabba.failed': createMoving(10, 50),
+         'logins.obiwan.failed': createMoving(10, 50)]
     }
 
     def createAllTimeQueue(long startValue = 900000, int increment = 20) {

f5c4f1ed18691ccbcbcd89bca8c52ce76fd6f2ed
Ville Svärd <ville.svard@agical.com>
2012-01-30 17:28:00 +0100

    First experiment with lazy collections

--- code/clj-client/src/client/core.clj
+++ code/clj-client/src/client/core.clj
@@ -1,7 +1,39 @@
 (ns client.core
-  (:import java.util.Date)
-  (:require [monotony.configured :as mc]))
+  (:import java.util.Date))
 
 (defn now []
-  (/ (System/currentTimeMillis) 1000))
+  (int (/ (System/currentTimeMillis) 1000)))
+
+(def units {:seconds 1
+            :minutes 60
+            :hours   3600})
+
+(defn in-seconds [n unit]
+  (* n (units unit)))
+
+(defn every [n unit]
+  (let [seconds (in-seconds n unit)]
+    (iterate (partial + seconds) 0)))
+
+(defn before [instant n unit]
+  (- instant (in-seconds n unit)))
+
+(defn from-onward [start s]
+  (map (partial + start) s))
+
+(defn metric [name f]
+  (fn [timestamp]
+    [name (f) timestamp]))
+
+(defn flat-line [value]
+  (repeat value))
+
+(defn constantly-increasing
+  ([]
+      (constantly-increasing 0 1))
+  ([by]
+     (constantly-increasing 0 by))
+  ([start by]
+     (iterate (partial + by) start)))
+
 
